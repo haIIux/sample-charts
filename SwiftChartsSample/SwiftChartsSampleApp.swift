d1d0d1f4ab7cbd79e69a924568d79410988e3192
hallux <halluxdev@gmail.com>
2022-11-30 15:35:02 -0500

    Initial Commit

---
+++ SwiftChartsSample/SwiftChartsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsSampleApp.swift
+//  SwiftChartsSample
+//
+//  Created by Rob Maltese on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
